0e2edb04ffc12350b600fe55585b72ed72dce2e2
Tanju Yıldız <ben@tanjuyildiz.com>
2022-05-29 22:01:40 +0300

    Create icon list

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home(props) {
+  const icons = props.icons;
   return (
     <div className={styles.container}>
       <Head>
@@ -14,7 +15,26 @@ export default function Home() {
         <h1 className={styles.title}>
           <span>Perilune</span> Icons
         </h1>
-        <p className={styles.description}>Simply beautiful open source icons</p>
+        <p className={styles.description}>
+          100+ Simply beautiful open source icons
+        </p>
+        <div className={styles.icons}>
+          {icons.map((icon) => (
+            <div
+              key={icon.key}
+              className={styles["icon-item"]}
+              title={icon.name}
+              data-tags="{icon.tags}"
+            >
+              <i
+                className={`${styles["icon-pl"]} pl pl-${icon.key}`}
+                aria-hidden="true"
+              ></i>
+              <code className={styles["icon-item-code"]}>pl-{icon.key}</code>
+              <small className={styles["icon-item-label"]}>{icon.name}</small>
+            </div>
+          ))}
+        </div>
       </main>
       <footer className={styles.footer}>
         <a
@@ -30,4 +50,17 @@ export default function Home() {
       </footer>
     </div>
   );
+}
+
+// Fetching data from the JSON file
+import fsPromises from "fs/promises";
+import path from "path";
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data.json");
+  const jsonData = await fsPromises.readFile(filePath);
+  const objectData = JSON.parse(jsonData);
+
+  return {
+    props: objectData,
+  };
 }
