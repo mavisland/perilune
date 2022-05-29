96d0ddaff2d3f23b7508eed8ac7c3bdcfdaf8bf0
Tanju Yıldız <ben@tanjuyildiz.com>
2022-05-29 20:22:03 +0300

    Create .fantasticonrc.js

---
+++ .fantasticonrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  inputDir: "./icons",
+  outputDir: "./font",
+  fontTypes: ["woff2", "woff"],
+  assetTypes: ["css", "scss", "json", "html"],
+  name: "perilune",
+  prefix: "pl",
+  selector: ".pl",
+  fontsUrl: "../fonts",
+  formatOptions: {
+    json: {
+      indent: 2,
+    },
+  },
+};
